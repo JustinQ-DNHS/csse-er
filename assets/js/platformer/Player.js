e690c9b8177676ccd06aba04bafc43d27c05a318
JustinQ-DNHS <justin.q224@icloud.com>
2023-12-13 10:00:30 -0800

    reduced jump height

--- assets/js/platformer/Player.js
+++ assets/js/platformer/Player.js
@@ -112,7 +112,7 @@ export class Player extends Character{
             if (this.movement.right) this.x += this.speed;  // Move to right
         }
         if (this.isGravityAnimation("w")) {
-            if (this.movement.down) this.y -= (this.bottom * .50);  // jump 33% higher than bottom
+            if (this.movement.down) this.y -= (this.bottom * .33);  // jump 33% higher than bottom
         } 
 
         //Prevents Player from leaving screen
